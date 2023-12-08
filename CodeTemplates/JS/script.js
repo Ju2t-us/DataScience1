eff520e0fe7b142932261a661cfce94fd3403bfe
Thatcher <jthatcher4@cherrycreekschools.org>
2023-12-08 10:38:49 -0700

    Add files

--- CodeTemplates/JS/script.js
+++ CodeTemplates/JS/script.js
@@ -1,1 +1,9 @@
-// JavaScript source code
+function fadeOutElements() {
+    // Get elements by their IDs
+    var all = document.getElementById('all');
+    var button = document.getElementById('button');
+
+    // Apply the fade-out effect by changing opacity
+    all.style.opacity = '0';
+    button.style.top = '100px';
+}
