1940b5ddb619935e96e2b248f2fd167f48807a5d
Thatcher <jthatcher4@cherrycreekschools.org>
2023-12-15 13:33:12 -0700

    final project update

--- Projects/Final Project/script_scene2.js
+++ Projects/Final Project/script_scene2.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const popup = document.getElementById("popup");
     const clickToContinue = document.getElementById("clickToContinue");
     const newBox = document.getElementById("newBox");
+    // Add reference for newBox2
+    const newBox2 = document.getElementById("newBox2");
 
     document.addEventListener("click", function () {
         // Show overlay and popup on click
@@ -11,11 +13,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Show "Click to continue" text after 20 seconds
         setTimeout(function () {
-            clickToContinue.style.display = "block";
+            clickToContinue.style.opacity = 1;
         }, 20000);
     });
 
-    // Add click event for sliding off the popup and showing new box
+    // Add click event for sliding off the popup and showing new boxes
     popup.addEventListener("click", function () {
         // Slide off to the left and disappear
         popup.style.animation = "slideOff 0.8s ease-out forwards";
@@ -25,5 +27,11 @@ document.addEventListener("DOMContentLoaded", function () {
             newBox.style.display = "block";
             newBox.style.animation = "slideIn 0.8s ease-in forwards";
         }, 800); // Adjust the timing if needed
+
+        // Show second new box after the first new box
+        setTimeout(function () {
+            newBox2.style.display = "block";
+            newBox2.style.animation = "slideIn2 0.8s ease-in forwards";
+        }, 1600); // Adjust the timing if needed
     });
 });
